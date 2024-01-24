de4a2f0a03faef0d629a6b3ce951df8eb7c6b3a4
Levi Zitting <me@levizitting.com>
2024-01-24 10:26:44 -0600

    feat; handle nested and generic dtos in swagger

--- ExampleBlogApi/Infrastructure/Swagger/SwaggerExtensions.cs
+++ ExampleBlogApi/Infrastructure/Swagger/SwaggerExtensions.cs
@@ -9,8 +9,25 @@ public static class SwaggerExtensions
 {
     public static SwaggerGenOptions ApplySwaggerSchemaCustomizations(this SwaggerGenOptions options)
     {
+        options.CustomSchemaIds(type =>
+        {
+            return GetSchemaIdForType(type);
+        });
         options.MapType<SortOption>(() => new OpenApiSchema { Type = "string", Default = new OpenApiString("fieldName:asc") });
         options.OperationFilter<NestedValueWithoutSuffixOperationFilter>();
         return options;
+    }
+
+    private static string GetSchemaIdForType(Type t)
+    {
+        if (t.IsGenericType)
+        {
+            var genericTypeDefinition = t.GetGenericTypeDefinition().Name;
+            genericTypeDefinition = genericTypeDefinition.Remove(genericTypeDefinition.IndexOf('`'));
+            var genericArguments = string.Join("_", t.GetGenericArguments().Select(GetSchemaIdForType));
+            return $"{genericTypeDefinition}Of{genericArguments}";
+        }
+
+        return t.IsNested && t.DeclaringType is not null ? $"{GetSchemaIdForType(t.DeclaringType)}_{t.Name}" : t.Name;
     }
 }
